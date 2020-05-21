a4502d011bdb5678ee5aff448134df92307a64a6
Deval Shah <devalshah1619@gmail.com>
2020-05-21 19:26:51 +0000

    First Commit

---
+++ timm/models/__init__.py
@@ -0,0 +1,8 @@
+from .efficientnet import *
+from .resnet import *
+
+from .registry import *
+from .factory import create_model
+from .helpers import load_checkpoint, resume_checkpoint
+from .layers import TestTimePoolHead, apply_test_time_pool
+from .layers import convert_splitbn_model
